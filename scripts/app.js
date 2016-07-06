3d3e5a717b8f4f75c304c22758a7ac0b93c30a03
nathanallen <thenathanator@gmail.com>
2016-07-06 08:44:04 -0700

    handlebars solution

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,9 @@ $(document).on('ready', function(){
 
   $('h1').text("Meet Mr. Fox"); // KEEP
 
+  var source = $("#profile-template").html(); // grab html string of profile template
+  var template = Handlebars.compile(source);  // create the jello-mold
+  var profile_html = template(data);          // pour in the jello (data)
+  $('.profile').html(profile_html);           // display jello in profile
+
 });
